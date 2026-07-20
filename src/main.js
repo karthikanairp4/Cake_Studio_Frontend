4cc135285fa36e7cf6ee2675a9db7fc46387612f
karthikanairp4 <prasannakumarkarthikanair@gmail.com>
2026-07-20 11:11:25 -0500

    Some more changes

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import './styles/theme.css'
 
 import App from './App.vue'
 import router from './router'
